fa8cdd5437cb4d350466b630062c5529813dc971
Your Name <kato.921002@gmail.com>
2023-09-09 16:14:59 +0900

    finish

--- grpc-lesson/client/main.go
+++ grpc-lesson/client/main.go
@@ -6,12 +6,20 @@ import (
 	"grpc-lesson/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	certFile := "/home/kato/.local/share/mkcert/rootCA.pem"
+	creds, err := credentials.NewClientTLSFromFile(certFile, "")
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("failed to connect", err)
 	}
@@ -20,11 +28,15 @@ func main() {
 	client := pb.NewFileServiceClient(conn)
 	//callListFiles(client)
 	callDownload(client)
+	//callUpload(client)
+	//callUploadAndNotifyProgress(client)
 }
 
 func callListFiles(client pb.FileServiceClient) {
+	md := metadata.New(map[string]string{"authorization": "Bearer test-token"})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
 	req := &pb.ListFilesRequest{}
-	res, err := client.ListFiles(context.Background(), req)
+	res, err := client.ListFiles(ctx, req)
 	if err != nil {
 		log.Fatalf("failed to invoke: %v", err)
 	}
@@ -33,10 +45,13 @@ func callListFiles(client pb.FileServiceClient) {
 }
 
 func callDownload(client pb.FileServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	req := &pb.DownloadRequest{
 		Filename: "name.txt",
 	}
-	stream, err := client.Download(context.Background(), req)
+	stream, err := client.Download(ctx, req)
 	if err != nil {
 		log.Fatalf("failed to invoke: %v", err)
 	}
@@ -48,10 +63,126 @@ func callDownload(client pb.FileServiceClient) {
 		}
 
 		if err != nil {
-			log.Fatalf("failed to receive chunk data: %v", err)
+			resErr, ok := status.FromError(err)
+			if ok {
+				if resErr.Code() == codes.NotFound {
+					log.Fatalf("error code: %v ", "error message: %v", resErr.Code(), resErr.Message())
+				} else if resErr.Code() == codes.DeadlineExceeded {
+					log.Fatalln("deadline exceeded")
+				} else {
+					log.Fatalln("unkown grpc error")
+				}
+			} else {
+				log.Fatalf("failed to receive chunk data: %v", err)
+			}
+
 		}
 
 		log.Printf("received chunk data: %v", res.GetData())
 		log.Printf("received chunk data: %v", string(res.GetData()))
 	}
+}
+
+func callUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/home/kato/Udemy/gRPC/grpc-lesson/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	defer file.Close()
+
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+
+	buf := make([]byte, 5)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed to read chunk data: %v", err)
+		}
+
+		req := &pb.UploadRequest{Data: buf[:n]}
+		sendErr := stream.Send(req)
+		if sendErr != nil {
+			log.Fatalf("failed to send chunk data: %v", err)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("failed to receive response: %v", err)
+	}
+	log.Printf("received response: %v", res.GetSize())
+
+}
+
+func callUploadAndNotifyProgress(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/home/kato/Udemy/gRPC/grpc-lesson/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	defer file.Close()
+
+	stream, err := client.UploadAndNotifyProgress(context.Background())
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+
+	//request
+	buf := make([]byte, 5)
+	go func() {
+		for {
+			n, err := file.Read(buf)
+			if n == 0 || err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("failed to read chunk data: %v", err)
+			}
+
+			req := &pb.UploadAndNotifyProgressRequest{Data: buf[:n]}
+			sendErr := stream.Send(req)
+			if sendErr != nil {
+				log.Fatalf("failed to send chunk data: %v", err)
+			}
+			time.Sleep(1 * time.Second)
+
+		}
+		err := stream.CloseSend()
+		if err != nil {
+			log.Fatalf("failed to close send: %v", err)
+		}
+	}()
+
+	//response
+	ch := make(chan struct{})
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("failed to receive chunk data: %v", err)
+			}
+			log.Printf("received chunk data: %v", res.GetMsg())
+		}
+		close(ch)
+	}()
+	<-ch
+
 }
--- grpc-lesson/server/main.go
+++ grpc-lesson/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"grpc-lesson/pb"
@@ -11,7 +12,12 @@ import (
 	"os"
 	"time"
 
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
@@ -48,6 +54,10 @@ func (*server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadS
 	filename := req.GetFilename()
 	path := "/home/kato/Udemy/gRPC/grpc-lesson/storage/" + filename
 
+	if _, err := os.Stat(path); err != nil {
+		return status.Error(codes.NotFound, "file not found")
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
 		return err
@@ -76,13 +86,101 @@ func (*server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadS
 	return nil
 }
 
+func (*server) Upload(stream pb.FileService_UploadServer) error {
+	fmt.Println("Upload invoked")
+
+	var buf bytes.Buffer
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			res := &pb.UploadResponse{Size: int32(buf.Len())}
+			return stream.SendAndClose(res)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		data := req.GetData()
+		log.Printf("received chunk data: %v", data)
+		log.Printf("received chunk data: %v", string(data))
+		buf.Write(data)
+	}
+}
+
+func (*server) UploadAndNotifyProgress(stream pb.FileService_UploadAndNotifyProgressServer) error {
+	fmt.Println("UploadAndNotifyProgress invoked")
+
+	size := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		data := req.GetData()
+		log.Printf("received chunk data: %v", data)
+		size += len(data)
+
+		res := &pb.UploadAndNotifyProgressResponse{
+			Msg: fmt.Sprintf("received %v bytes", size),
+		}
+		err = stream.Send(res)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func myLogging() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+		log.Printf("request data: %v", req)
+		resp, err = handler(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		log.Printf("response data: %v", resp)
+		return resp, nil
+	}
+}
+
+func authorize(ctx context.Context) (context.Context, error) {
+	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	if err != nil {
+		return nil, err
+	}
+
+	if token != "test-token" {
+		return nil, status.Error(codes.Unauthenticated, "token is invalid")
+	}
+
+	return ctx, nil
+}
+
 func main() {
 	lis, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	creds, err := credentials.NewServerTLSFromFile("ssl/localhost.pem", "ssl/localhost-key.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s := grpc.NewServer(
+		grpc.Creds(creds),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				myLogging(),
+				grpc_auth.UnaryServerInterceptor(authorize))))
+
 	pb.RegisterFileServiceServer(s, &server{})
 
 	fmt.Println("start server")
